dec02c4affb777ffe9816cd34f570d0624c45c34
Omar Rizwan <omar.rizwan@gmail.com>
2015-06-27 17:02:17 -0700

    Initial Commit

---
+++ Screenotate/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Screenotate
+//
+//  Created by Omar Rizwan on 6/27/15.
+//  Copyright (c) 2015 Omar Rizwan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
